b8de0250a4029d9f0718e93b6cb7d0e04daa8862
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-04-27 14:26:06 +0200

    Add githack

--- build_prod.sh
+++ build_prod.sh
@@ -1,3 +1,16 @@
 bundle exec jekyll build --config=_config.yml,_config.prod.yml
-git subtree split --branch deploy --prefix _site/
+
+STATUS="$(git status)"
+
+if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
+then
+    sed -i "" '/_site/d' ./.gitignore
+    git add .
+    git commit -m "Edit .gitignore to publish"
+    git push origin `git subtree split --prefix _site master`:deploy --force
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "Need clean working directory to publish"
+fi
 
